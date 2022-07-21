39b4621c6befce771a3d1efa0091b930511538b2
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-07-21 16:57:15 +0300

    third commit

--- api/script.js
+++ api/script.js
@@ -1,10 +1,12 @@
 const btnTextGetir = document.getElementById('btn-text-get');
-const btnJSONGetir = document.getElementById('btn-json-data-add');
+const btnJSONGetir = document.getElementById('btn-json-get');
 const btnApidenJSONGetir = document.getElementById('btn-api-json-get');
+const btnJSONadd = document.getElementById('btn-json-data-add');
 
 btnTextGetir.addEventListener('click', getText);
 btnJSONGetir.addEventListener('click', getJSON);
 btnApidenJSONGetir.addEventListener('click', getJSONfromAPI);
+btnJSONadd.addEventListener('click', jsonAddData);
 
 const sonucDiv = document.getElementById('sonuc');
 
@@ -27,7 +29,23 @@ function getJSONfromAPI(e) {
 function ekranaYazdir(data) {
     let cikti = '';
     data.forEach(user => {
-        cikti += `<p>${user.name}<p>`
+        cikti += `<span>${user.name}<span><br>`
     });
     sonucDiv.innerHTML = cikti;
+}
+
+function jsonAddData(e) {
+    e.preventDefault();
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'deneme',
+            body: 'body alani',
+            userId: 5
+        }),
+        headers: {'Content-Type': 'application/json'}
+    })
+        .then(response => response.json())
+        .then(sonucx => console.log(sonucx));
 }
